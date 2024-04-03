cab3faf2593c8e6ca54c7722600aa1c1605edca7
hauct <cungtronghau@gmail.com>
2024-04-04 00:16:29 +0700

    edit something

--- jobs/spark-city.py
+++ jobs/spark-city.py
@@ -34,3 +34,61 @@ def main():
     ])
 
     # Gps schema
+    gps_schema = StructType([
+        StructField('id', StringType(), True),
+        StructField('device_id', StringType(), True),
+        StructField('timestamp', TimeStampType(), True),
+        StructField('speed', DoubleType(), True),
+        StructField('vehicle_type', StringType(), True)  
+    ])
+
+   # Traffic schema
+    traffic_schema = StructType([
+        StructField('id', StringType(), True),
+        StructField('device_id', StringType(), True),
+        StructField('camera_id', StringType(), True),
+        StructField('location', StringType(), True),
+        StructField('timestamp', TimeStampType(), True),
+        StructField('snapshot', StringType(), True)
+    ])
+   
+   # Weather schema
+    weather_schema = StructType([
+        StructField('id', StringType(), True),
+        StructField('device_id', StringType(), True),
+        StructField('timestamp', TimeStampType(), True),
+        StructField('temperature', DoubleType(), True),
+        StructField('weather_condition', StringType(), True),
+        StructField('precipitation', DoubleType(), True),
+        StructField('wind_speed', DoubleType(), True),
+        StructField('humidity', IntegerType(), True),
+        StructField('air_quality_index', DoubleType(), True),
+    ])
+    
+   # Emergency schema
+    emergency_schema = StructType([
+        StructField('id', StringType(), True),
+        StructField('device_id', StringType(), True),
+        StructField('incident_id', TimeStampType(), True),
+        StructField('type', StringType(), True),
+        StructField('timestamp', TimeStampType(), True),
+        StructField('location', StringType(), True),
+        StructField('status', StringType(), True),
+        StructField('description', StringType(), True)
+    ])
+    
+    def read_kafka_topic(topic, schema):
+        return (spark.readStream
+                .format('kafka')
+                .option('kafka.bootsrap.servers', 'broker:29092')
+                .option('subcribe', topic)
+                .option('startingOffsets', 'earliest')
+                .load()
+                .selectExpr('CAST(values as STRING)')
+                .select(from_json(col('value'), schema).alias('data'))
+                .select('data.*')
+                .withWatermark('timestamp', '2 minutes'))
+    
+    vehicle_df = read_kafka_topic('vehicle_data', vehicle_schema).alias('vehicle')
+    vehicle_df = read_kafka_topic('vehicle_data', vehicle_schema).alias('vehicle')
+    vehicle_df = read_kafka_topic('vehicle_data', vehicle_schema).alias('vehicle')
